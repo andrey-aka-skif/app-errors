7212838cda7de501458d0eddf0795b293bc9ea5b
andrey-aka-skif <2119163@gmail.com>
2025-07-27 18:59:55 +0300

    feature: добавлены билдеры ошибок для Axios и Superagent
    * Добавлены файлы axios-error-builder.js и superagent-error-builder.js для стандартизации обработки ошибок в HTTP-клиентах Axios и Superagent.
    * Обновлён файл index.js для экспорта функций fromAxios и fromSuperagent для упрощения интеграции.

---
+++ src/axios-error-builder.js
@@ -0,0 +1,73 @@
+import DisconnectedError from './disconnected-error'
+import BadRequestError from './bad-request-error'
+import UnauthorizedError from './unauthorized-error'
+import ForbiddenError from './forbidden-error'
+import NotFoundError from './not-found-error'
+import ConflictError from './conflict-error'
+import InternalServerError from './internal-server-error'
+import UnknownError from './unknown-error'
+
+/**
+ * Попробовать получить значения для свойства "detail" из ответа сервера.
+ * Проверяет несколько вариантов доступа к данным: "data.detail", "data.Detail", "data".
+ * @param {*} response - Ответ сервера
+ * @returns {*} Значение "detail", если оно существует, иначе null
+ */
+const tryGetDetail = response => {
+  return (
+    response?.data?.detail ?? response?.data?.Detail ?? response?.data ?? null
+  )
+}
+
+/**
+ * Преобразует серверную ошибку в соответствующий класс ошибки.
+ * Сопоставляет статус-код ответа с конкретным типом ошибки.
+ * @param {*} error - Объект ошибки с серверным ответом
+ * @returns {BaseAppError} Экземпляр соответствующего класса ошибки
+ */
+const getServerError = error => {
+  const detail = tryGetDetail(error.response)
+
+  switch (error.response.status) {
+    case 400:
+      return new BadRequestError(detail)
+    case 401:
+      return new UnauthorizedError(detail)
+    case 403:
+      return new ForbiddenError(detail)
+    case 404:
+      return new NotFoundError(detail)
+    case 409:
+      return new ConflictError(detail)
+    case 500:
+      return new InternalServerError(detail)
+    default:
+      return new UnknownError()
+  }
+}
+
+/**
+ * Основная функция преобразования ошибок Axios в стандартные ошибки приложения.
+ * Выполняет последовательную проверку типов ошибок:
+ * 1. Проверка, является ли ошибка Axios (error.isAxiosError)
+ * 2. Наличие ответа от сервера (error.response)
+ * 3. Наличие запроса без ответа (error.request)
+ * Возвращает соответствующий класс ошибки на основе результатов проверок.
+ * @param {*} error - Исходная ошибка от Axios
+ * @returns {BaseAppError} Экземпляр одного из классов ошибок
+ */
+export const fromAxios = error => {
+  if (!error.isAxiosError) {
+    return new UnknownError()
+  }
+
+  if (error.response) {
+    return getServerError(error)
+  }
+
+  if (error.request) {
+    return new DisconnectedError()
+  }
+
+  return new UnknownError()
+}
