24b990cef509495709d778fdad02ec175699f216
andrey-aka-skif <2119163@gmail.com>
2025-07-28 14:06:24 +0300

    refactor: билдеры ошибок перенесены в каталог builders
    * Файлы axios-error-builder.js и superagent-error-builder.js перенесены в каталог builders

--- src/builders/superagent-error-builder.js
+++ src/builders/superagent-error-builder.js
@@ -1,11 +1,11 @@
-import DisconnectedError from './disconnected-error'
-import BadRequestError from './bad-request-error'
-import UnauthorizedError from './unauthorized-error'
-import ForbiddenError from './forbidden-error'
-import NotFoundError from './not-found-error'
-import ConflictError from './conflict-error'
-import InternalServerError from './internal-server-error'
-import UnknownError from './unknown-error'
+import DisconnectedError from '../disconnected-error'
+import BadRequestError from '../bad-request-error'
+import UnauthorizedError from '../unauthorized-error'
+import ForbiddenError from '../forbidden-error'
+import NotFoundError from '../not-found-error'
+import ConflictError from '../conflict-error'
+import InternalServerError from '../internal-server-error'
+import UnknownError from '../unknown-error'
 
 /**
  * Проверка на возможную ошибку приложения.
