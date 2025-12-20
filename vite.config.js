df6fa109f09567405f203dfa6442287d132f25a4
andrey-aka-skif <2119163@gmail.com>
2025-12-20 18:54:20 +0300

    fix: исправлена ошибка в конфигурации vite
    Версия 2 не использует единую точку входа index.js, поэтому появляется некоторая магия в настройке vite (preserveEntrySignatures, sourcemap и т.д).
    
    https://github.com/andrey-aka-skif/app-errors/issues/23

--- vite.config.js
+++ vite.config.js
@@ -1,52 +1,36 @@
-import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
-
-const entries = {
-  'error-types': fileURLToPath(new URL('./src/errorTypes.js', import.meta.url)),
-  logic: fileURLToPath(new URL('./src/logicAppError.js', import.meta.url)),
-  axios: fileURLToPath(
-    new URL('./src/axiosNetworkAppError.js', import.meta.url)
-  ),
-  superagent: fileURLToPath(
-    new URL('./src/superagentNetworkAppError.js', import.meta.url)
-  ),
-}
+import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-    },
-  },
   build: {
     rollupOptions: {
-      input: entries,
-      output: [
-        {
-          dir: 'dist/es',
-          format: 'es',
-          entryFileNames: '[name].js',
-          preserveModules: false,
-          exports: 'named',
-        },
-        {
-          dir: 'dist/umd',
-          format: 'umd',
-          entryFileNames: '[name].umd.js',
-          name: 'appErrors',
-          globals: {},
-          exports: 'named',
-        },
-      ],
-      external: [],
-    },
-    minify: 'terser',
-    terserOptions: {
-      format: {
-        comments: 'some',
+      input: {
+        'error-types': fileURLToPath(
+          new URL('./src/errorTypes.js', import.meta.url)
+        ),
+        logic: fileURLToPath(
+          new URL('./src/logicAppError.js', import.meta.url)
+        ),
+        axios: fileURLToPath(
+          new URL('./src/axiosNetworkAppError.js', import.meta.url)
+        ),
+        superagent: fileURLToPath(
+          new URL('./src/superagentNetworkAppError.js', import.meta.url)
+        ),
+      },
+
+      preserveEntrySignatures: 'strict',
+
+      output: {
+        dir: 'dist',
+        format: 'es',
+        preserveModules: true,
+        preserveModulesRoot: 'src',
+        entryFileNames: '[name].js',
       },
     },
-    sourcemap: true,
+
+    sourcemap: false,
     emptyOutDir: true,
   },
 })
