e9beeee1e87701c8963a76cfe77aa95fdcafcb6a
andrey-aka-skif <2119163@gmail.com>
2025-12-20 19:02:14 +0300

    fix: удалены ссылки на vitest
    Т.к. в версии 2 не используются тесты.
    
    https://github.com/andrey-aka-skif/app-errors/issues/23

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,6 @@
 import { defineConfig, globalIgnores } from 'eslint/config'
 import globals from 'globals'
 import js from '@eslint/js'
-import pluginVitest from '@vitest/eslint-plugin'
 import eslintConfigPrettier from 'eslint-config-prettier'
 
 export default defineConfig([
@@ -21,16 +20,6 @@ export default defineConfig([
   },
 
   js.configs.recommended,
-
-  {
-    ...pluginVitest.configs.recommended,
-    files: [
-      'tests/**/*',
-      '**/*.test.js',
-      '**/*.spec.js',
-      'src/**/__tests__/**/*',
-    ],
-  },
 
   eslintConfigPrettier,
 
